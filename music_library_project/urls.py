9ce3d289cb0b1fd4e32d16917d03cce097136822
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-04-07 11:45:09 -0400

    Established database connection.

--- music_library_project/urls.py
+++ music_library_project/urls.py
@@ -1,4 +1,4 @@
-"""music_library URL Configuration
+"""music_library_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
