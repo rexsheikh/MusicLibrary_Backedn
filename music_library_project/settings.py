861bb16bce14dddb965b3fecea82eaaa933eb799
rexsheikh <56654312+rexsheikh@users.noreply.github.com>
2022-04-07 12:46:01 -0400

    created the songs app with title, artist, album, release_date and genre fields.

--- music_library_project/settings.py
+++ music_library_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'music_library_project.urls'
@@ -118,3 +120,6 @@ try:
     from music_library_project.local_settings import *
 except ImportError:
     pass
+
+
+CORS_ORIGIN_ALLOW_ALL = True
